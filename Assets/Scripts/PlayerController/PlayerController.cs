aacae72c1ada3f62373a3175ec1945d3e68b264c
MikeJoester <dan.nguyen200206@vnuk.edu.vn>
2022-11-16 14:23:41 +0700

    add: character movements + attack anim

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -5,34 +5,74 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeed = 4f;
+    [SerializeField] GameObject dust;   
     private Rigidbody2D rb;
     private Animator animator;
-    private Vector2 movement;
-    private bool isRight = true;    
+
+    // private Vector2 movement;
+    private bool isRight = true;
+    private float xVal;
+    private float yVal;
+    private bool attacking = false;
+     
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
     }
 
+    // public float xValue {
+    //     get { return xVal; }
+    // }
+
+    // public Vector2 playerMovement {
+    //     get { return movement; }
+    // }
+
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        if (movement.x > 0 && !isRight) {
+        xVal = Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed;
+        yVal = Input.GetAxisRaw("Vertical") * Time.deltaTime * moveSpeed;
+
+        //Flip function
+        if (xVal > 0 && !isRight) {
             Flip();
         }
-        if (movement.x < 0 && isRight) {
+        if (xVal < 0 && isRight) {
             Flip();
+        }
+        animator.SetFloat("Speed", Mathf.Abs(xVal));
+
+        //Move Function
+        var pos = new Vector2(transform.position.x + xVal, transform.position.y + yVal);
+        transform.position = Vector3.Lerp(pos, transform.position, 0.1f);
+
+        //Generate dust particles every x frame
+        if (xVal != 0 || yVal != 0) {
+            if (Time.frameCount % 20 == 0) {
+                GameObject instance = (GameObject)Instantiate(dust, transform.position, Quaternion.identity);
+                Destroy(instance, 0.8f);
+            }
+        }
+
+        //Attack trigger
+        if ((Input.GetKeyDown("z")) && (attacking == false)) {
+            attacking = true;
+            StartCoroutine(AtkDelay());
         }
     }
 
-    void FixedUpdate() {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    IEnumerator AtkDelay() {
+        animator.SetBool("isAttacking", true);
+        yield return new WaitForSeconds(0.5f);
+        attacking = false;
+        animator.SetBool("isAttacking", false);
+        yield return new WaitForSeconds(0.5f);
     }
 
     private void Flip() {
