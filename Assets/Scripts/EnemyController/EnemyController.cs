b9e948624efd9693c2e9b284ae16d1f461d2c08b
MikeJoester <dan.nguyen200206@vnuk.edu.vn>
2022-11-29 10:43:50 +0700

    add: UI and chest controller

---
+++ Assets/Scripts/EnemyController/EnemyController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Animator anim;
+    private float timer = 0f;
+    [SerializeField] float speed;
+    private float xVal = -1;
+    // private bool hostility = false;
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        timer += Time.deltaTime;
+        anim.SetFloat("runSpeed", Mathf.Abs(xVal));
+        if (timer >= 2f) {
+            // StartCoroutine(Rest());
+            xVal = xVal * -1;
+            Flip();
+            timer = 0f;
+        }
+        body.velocity = new Vector2(xVal * speed, body.velocity.y);
+    }
+
+    // IEnumerator Rest() {
+    //     anim.SetFloat("runSpeed", 0f);
+    //     yield return new WaitForSeconds(1f);
+    // }
+    private void Flip() {
+        Vector2 currentScale = this.gameObject.transform.localScale;
+        currentScale.x *= -1;
+        this.gameObject.transform.localScale = currentScale;
+    }
+}
---
+++ Assets/Scripts/ItemController/ChestTrigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestTrigger : MonoBehaviour
+{
+    private Animator anim;
+    private bool collided;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>(); 
+        collided = false;  
+    }
+
+    void Update() {
+        if ((collided) && (Input.GetKeyDown(KeyCode.E))) {
+            anim.SetBool("isOpen", true);
+        }  
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if((other.gameObject.tag == "Player")) {
+            collided = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if((other.gameObject.tag == "Player")) {
+            collided = false;
+        }
+    }
+}
