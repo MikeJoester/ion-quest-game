8cf3bc984d25abd3b10704fc919b26e8a9746c81
MikeJoester <dan.nguyen200206@vnuk.edu.vn>
2022-12-08 15:39:39 +0700

    .

---
+++ Assets/Scripts/DialogController/DialogTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    private bool triggerEntered = false;
+    public GameObject DialogueScreen;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E) && triggerEntered == true) {
+            DialogueScreen.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Player") {
+            triggerEntered = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider) {
+        triggerEntered = false;
+    }
+}
